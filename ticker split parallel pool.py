fa39d053876afbc4c4466a0823d9a20234826569
ar1515 <anirudh.ramesh@rutgers.edu>
2019-11-20 11:38:34 -0500

    changed code to read arguments from cmd line

--- ticker split parallel pool.py
+++ ticker split parallel pool.py
@@ -2,45 +2,56 @@ import pandas as pd
 import os
 from multiprocessing import Pool
 from multiprocessing.pool import ThreadPool
+from sys import argv
 
 
-# column_names = ['DATE', 'TIME_M', 'EX', 'BID', 'BIDSIZ', 'ASK', 'ASKSIZ', 'SYM_ROOT', 'SYM_SUFFIX']
-# output_file_path = 'taq_aug_2019_quotes_parallel'
-
-column_names = ['DATE', 'TIME_M', 'EX', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID']
-output_file_path = 'taq_aug_2019_trades'
+output_file_path = argv[2]
+# output_file_path = 'taq_sep_2019_trades'
+if 'quotes' in output_file_path:
+    column_names = ['DATE', 'TIME_M', 'EX', 'BID', 'BIDSIZ', 'ASK', 'ASKSIZ', 'SYM_ROOT', 'SYM_SUFFIX']
+else:
+    column_names = ['DATE', 'TIME_M', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID']
 
 
 def file_writer(chunk):
+    # print(chunk.SYM_ROOT.unique())
     chunk.columns = column_names
+    print(chunk.SYM_ROOT.unique())
     for ticker in chunk.SYM_ROOT.unique():
+        print(ticker)
         if os.path.exists(os.path.join(output_file_path, ticker+'.csv')):
             header = False
         else:
             header = True
         with open(os.path.join(output_file_path, ticker+'.csv'), 'a') as f:
+            # print(ticker+'.csv')
             chunk[chunk.SYM_ROOT == ticker].to_csv(f, index=False, header=header)
             f.close()
 
 
 def chunk_splitter(main_file, chunksize, skiprows, per_process_chunk):
-    print(skiprows)
     with ThreadPool(processes=10) as pool:
-        pool.imap(file_writer, pd.read_csv(main_file, skiprows=skiprows, chunksize=chunksize,
+        print(skiprows)
+        results = pool.imap(file_writer, pd.read_csv(main_file, skiprows=skiprows, chunksize=chunksize,
                                            nrows=chunksize*per_process_chunk, header=None))
         pool.close()
         pool.join()
+    # for r in results:
+    #     print(r)
 
 
 def main():
-    # main_file = 'taq_aug_2019_quotes_500_tickers.csv'
-    main_file = 'taq_aug_2019_trades_500_tickers.csv'
+    # main_file = 'taq_sep_2019_quotes_500_tickers.csv'
+    # main_file = 'taq_sep_2019_trades_500_tickers.csv'
+    # print(argv[1], argv[2], argv[3])
+    main_file = argv[1]
 
     chunksize = 10 ** 6
     per_process_chunk = 10 * 5
     skiprows = 1
-    total_rows = 284191154
-    # total_rows = 100000000
+    total_rows = int(argv[3])
+    # total_rows = chunksize*per_process_chunk+skiprows
+    # total_rows = 2814191154
 
     with Pool(processes=5, maxtasksperchild=1) as pool:
         pool.starmap(chunk_splitter, [(main_file, chunksize, s, per_process_chunk) for s in
