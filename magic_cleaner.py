71fb746b63a0bc91c69d87aafcd781462e3e8a50
ar1515 <anirudh.ramesh@rutgers.edu>
2019-11-26 14:56:26 -0500

    random changes and moving things around. columns for aug trades had to be readjusted to exclude EX

--- magic_cleaner.py
+++ magic_cleaner.py
@@ -2,11 +2,12 @@ from os import system, walk, path
 from multiprocessing import Pool, Queue, Process
 import pandas as pd
 from sys import argv
+from numpy import any, array
 
 
 print_queue = Queue()
-quotes_columns = ['DATE', 'TIME_M', 'EX', 'BID', 'BIDSIZ', 'ASK', 'ASKSIZ', 'SYM_ROOT', 'SYM_SUFFIX']
-trades_columns = ['DATE', 'TIME_M', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID']
+quotes_columns = array(['DATE', 'TIME_M', 'EX', 'BID', 'BIDSIZ', 'ASK', 'ASKSIZ', 'SYM_ROOT', 'SYM_SUFFIX'])
+trades_columns = array(['DATE', 'TIME_M', 'EX', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID'])
 
 
 def printer():
@@ -48,11 +49,16 @@ def sorter(original_file):
     system('rm ' + temp_file)
 
 
-def checker(file, chunksize):
-    all_chunks = pd.read_csv(file, chunksize=chunksize)
-    first_chunk = all_chunks.get()
-    if (('quotes' in file) and (first_chunk.columns != quotes_columns)) or (first_chunk.columns != trades_columns):
-        print_queue.put('Column mismatch: ' + file)
+def checker(file):
+    # print_queue.put(file)
+    all_chunks = pd.read_csv(file, chunksize=10**6)
+    first_chunk = next(all_chunks)
+    try:
+        if (('quotes' in file) and any(first_chunk.columns != quotes_columns)) or (('trades' in file) and any(first_chunk.columns != trades_columns)):
+            print_queue.put('Column mismatch: ' + file)
+            return
+    except ValueError:
+        print_queue.put('Size difference: ' + file + str(first_chunk.columns))
         return
     ticker = first_chunk.SYM_ROOT.unique()[0]
     for chunk in all_chunks:
@@ -62,25 +68,36 @@ def checker(file, chunksize):
 
 
 def sort_caller():
-    original_path = argv[1]
-    print(original_path)
-    files = next(walk(original_path))[2]
-    pool = Pool(processes=30, maxtasksperchild=3)
-    results = pool.map_async(sorter, [path.join(original_path, f) for f in files])
-    pool.close()
-    pool.join()
-    print(results.get())
+    all_pools = []
+    results = []
+    for i in range(1, len(argv)):
+        original_path = argv[i]
+        # print(original_path)
+        files = next(walk(original_path))[2]
+        all_pools.append(Pool(processes=10, maxtasksperchild=3))
+        results.append(all_pools[-1].map_async(sorter, [path.join(original_path, f) for f in files]))
+        all_pools[-1].close()
+
+    for i in range(len(all_pools)):
+        all_pools[i].join()
+        print_queue.put(results[i].get())
 
 
 def check_caller():
-    original_path = argv[1]
-    # print(original_path)
-    files = next(walk(original_path))[2]
-    pool = Pool(processes=30, maxtasksperchild=3)
-    results = pool.map_async(checker, [path.join(original_path, f) for f in files])
-    pool.close()
-    pool.join()
-    print(results.get())
+    all_pools = []
+    results = []
+    for i in range(1, len(argv)):
+        original_path = argv[i]
+        print(original_path)
+        files = next(walk(original_path))[2]
+        # print_queue.put(files)
+        all_pools.append(Pool(processes=10, maxtasksperchild=3))
+        results.append(all_pools[-1].map_async(checker, [path.join(original_path, f) for f in files]))
+        all_pools[-1].close()
+
+    for i in range(len(all_pools)):
+        all_pools[i].join()
+        print_queue.put(results[i].get())
 
 
 def main():
@@ -88,6 +105,7 @@ def main():
     printer_process.start()
 
     check_caller()
+    # sort_caller()
 
     print_queue.put('fin')
     printer_process.join()
--- ticker split parallel pool.py
+++ ticker split parallel pool.py
@@ -5,22 +5,24 @@ from multiprocessing.pool import ThreadPool
 from sys import argv
 
 
-output_file_path = argv[2]
-# output_file_path = 'taq_sep_2019_trades'
+# output_file_path = argv[2]
+output_file_path = 'taq_aug_2019_trades_1'
 if 'quotes' in output_file_path:
     column_names = ['DATE', 'TIME_M', 'EX', 'BID', 'BIDSIZ', 'ASK', 'ASKSIZ', 'SYM_ROOT', 'SYM_SUFFIX']
 else:
-    column_names = ['DATE', 'TIME_M', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID']
+    column_names = ['DATE', 'TIME_M', 'EX', 'SYM_ROOT', 'SYM_SUFFIX', 'SIZE', 'PRICE', 'TR_ID']
 
 
 def file_writer(chunk):
     # print(chunk.SYM_ROOT.unique())
     chunk.columns = column_names
-    print(chunk.SYM_ROOT.unique())
+    # print(chunk.SYM_ROOT.unique())
     for ticker in chunk.SYM_ROOT.unique():
         print(ticker)
         if os.path.exists(os.path.join(output_file_path, ticker+'.csv')):
             header = False
+            while os.path.getsize(os.path.join(output_file_path, ticker+'.csv')) == 0:
+                pass
         else:
             header = True
         with open(os.path.join(output_file_path, ticker+'.csv'), 'a') as f:
@@ -36,28 +38,31 @@ def chunk_splitter(main_file, chunksize, skiprows, per_process_chunk):
                                            nrows=chunksize*per_process_chunk, header=None))
         pool.close()
         pool.join()
-    # for r in results:
-    #     print(r)
+    for r in results:
+        print(r)
 
 
 def main():
     # main_file = 'taq_sep_2019_quotes_500_tickers.csv'
-    # main_file = 'taq_sep_2019_trades_500_tickers.csv'
+    main_file = 'archive/taq_aug_2019_trades_500_tickers.csv'
     # print(argv[1], argv[2], argv[3])
-    main_file = argv[1]
+    # main_file = argv[1]
 
     chunksize = 10 ** 6
     per_process_chunk = 10 * 5
     skiprows = 1
-    total_rows = int(argv[3])
+    # total_rows = int(argv[3])
     # total_rows = chunksize*per_process_chunk+skiprows
-    # total_rows = 2814191154
+    total_rows = 314208630
 
     with Pool(processes=5, maxtasksperchild=1) as pool:
-        pool.starmap(chunk_splitter, [(main_file, chunksize, s, per_process_chunk) for s in
+        result = pool.starmap(chunk_splitter, [(main_file, chunksize, s, per_process_chunk) for s in
                                       range(skiprows, total_rows, chunksize*per_process_chunk)])
         pool.close()
         pool.join()
+
+    # for r in result:
+    #     print(r)
 
 
 if __name__ == '__main__':
