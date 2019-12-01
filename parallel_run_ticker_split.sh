a15b173feeaa226a49254d7329b63ee823e9ee09
ar1515 <anirudh.ramesh@rutgers.edu>
2019-12-01 01:15:44 -0500

    rewrote parallel job committer to have only 1 task call total.

--- parallel_run_ticker_split.sh
+++ parallel_run_ticker_split.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#SBATCH --time=10:00:00 --mem=20GB --ntasks=1 --cpus-per-task=4 --output='ticker_split_parallel.out' --error='ticker_split_parallel.err'
+#SBATCH --time=10:00:00 --mem=30GB --ntasks=2 --cpus-per-task=4 --output='ticker_split_parallel.out' --error='ticker_split_parallel.err'
 
 python3 ticker\ split\ parallel\ pool.py
